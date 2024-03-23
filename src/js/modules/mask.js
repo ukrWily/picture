8b0c58aa0458d67b3d24154a58d037b470e8f9a0
”ukrWily” <ukrwily@gmail.com>
2024-03-23 18:59:11 +0200

    PC mask 23/3

---
+++ src/js/modules/mask.js
@@ -0,0 +1,22 @@
+const mask = (selector) => {
+  function createMask(event) {
+    let matrix = "+38 (___) ___ __ __",
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, "");
+
+    if (def.length >= val.length) {
+      val = def;
+    }
+
+    this.value = matrix.replace(/./g, function (a) {
+      return /[_\d]/.test(a) && i < val.length
+        ? val.charAt(i++)
+        : i >= val.length
+        ? ""
+        : a;
+    });
+  }
+};
+
+export default mask;
