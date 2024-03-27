e871efd68bb5e42498284a870f1225ace4f5dadf
ukrWily <ukrwily@gmail.com>
2024-03-27 13:34:49 +0200

    L showMoreStyles

--- dist/script.js
+++ dist/script.js
@@ -321,9 +321,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-const showMoreStyles = (trigger, styles) => {
-  const cards = document.querySelectorAll(styles),
-    btn = document.querySelector(trigger);
+/* harmony import */ var _services_requests__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../services/requests */ "./src/js/services/requests.js");
+
+const showMoreStyles = (trigger, wrapper) => {
+  const btn = document.querySelector(trigger);
 
   // cards.forEach((card) => {
   //   card.classList.add("animated", "fadeInUp");
@@ -333,7 +334,7 @@ const showMoreStyles = (trigger, styles) => {
   //   cards.forEach((card) => {
   //     card.classList.remove("hidden-lg", "hidden-md", "hidden-sm", "hidden-xs");
   //     card.classList.add(
-  //       "col-sm-3",
+  //       "col-sm-3",v
   //       "col-sm-offset-0",
   //       "col-xs-10",
   //       "col-xs-offset-1"
@@ -342,11 +343,37 @@ const showMoreStyles = (trigger, styles) => {
   //   // btn.style.display = "none";
   //   btn.remove();
   // });
+
+  btn.addEventListener("click", () => {
+    (0,_services_requests__WEBPACK_IMPORTED_MODULE_0__.getResource)("http://localhost:3000/styles").then(res => createCards(res)).catch(error => console.log(error));
+  });
+  function createCards(response) {
+    response.forEach(item => {
+      let card = document.querySelector("div");
+      card.classList.add("animated", "fadeInUp", "col-sm-3", "col-sm-offset-0", "col-xs-10", "col-xs-offset-1");
+      card.innerHTML = `
+      	<div class="styles-block">
+          <img src=${item.src} alt="style">
+          <h4>${item.title}</h4>
+          <a href=${item.link}>Подробнее</a>
+        </div>
+      `;
+      document.querySelector(wrapper).appendChild(card);
+    });
+  }
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (showMoreStyles);
 
 // "hidden-lg hidden-md hidden-sm hidden-xs styles-2"
 // col-sm-3 col-sm-offset-0 col-xs-10 col-xs-offset-1
+
+// <div class="hidden-lg hidden-md hidden-sm hidden-xs styles-2">
+// 	<div class=styles-block>
+// 		<img src=assets/img/styles-6.jpg alt>
+// 		<h4>Поп-арт</h4>
+// 		<a href="#">Подробнее</a>
+// 	</div>
+// </div>
 
 /***/ }),
 
@@ -543,7 +570,7 @@ window.addEventListener("DOMContentLoaded", () => {
   (0,_modules_mask__WEBPACK_IMPORTED_MODULE_3__["default"])('[name="phone"]');
   (0,_modules_checkTextInputs__WEBPACK_IMPORTED_MODULE_4__["default"])('[name="name"]');
   (0,_modules_checkTextInputs__WEBPACK_IMPORTED_MODULE_4__["default"])('[name="message"]');
-  (0,_modules_showMoreStyles__WEBPACK_IMPORTED_MODULE_5__["default"])(".button-styles", ".styles-2");
+  (0,_modules_showMoreStyles__WEBPACK_IMPORTED_MODULE_5__["default"])(".button-styles", "#styles .row");
 });
 })();
 
--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,6 +1,7 @@
-const showMoreStyles = (trigger, styles) => {
-  const cards = document.querySelectorAll(styles),
-    btn = document.querySelector(trigger);
+import { getResource } from "../services/requests";
+
+const showMoreStyles = (trigger, wrapper) => {
+  const btn = document.querySelector(trigger);
 
   // cards.forEach((card) => {
   //   card.classList.add("animated", "fadeInUp");
@@ -10,7 +11,7 @@ const showMoreStyles = (trigger, styles) => {
   //   cards.forEach((card) => {
   //     card.classList.remove("hidden-lg", "hidden-md", "hidden-sm", "hidden-xs");
   //     card.classList.add(
-  //       "col-sm-3",
+  //       "col-sm-3",v
   //       "col-sm-offset-0",
   //       "col-xs-10",
   //       "col-xs-offset-1"
@@ -19,9 +20,47 @@ const showMoreStyles = (trigger, styles) => {
   //   // btn.style.display = "none";
   //   btn.remove();
   // });
+
+  btn.addEventListener("click", () => {
+    getResource("http://localhost:3000/styles")
+      .then((res) => createCards(res))
+      .catch((error) => console.log(error));
+  });
+
+  function createCards(response) {
+    response.forEach((item) => {
+      let card = document.querySelector("div");
+
+      card.classList.add(
+        "animated",
+        "fadeInUp",
+        "col-sm-3",
+        "col-sm-offset-0",
+        "col-xs-10",
+        "col-xs-offset-1"
+      );
+
+      card.innerHTML = `
+      	<div class="styles-block">
+          <img src=${item.src} alt="style">
+          <h4>${item.title}</h4>
+          <a href=${item.link}>Подробнее</a>
+        </div>
+      `;
+      document.querySelector(wrapper).appendChild(card);
+    });
+  }
 };
 
 export default showMoreStyles;
 
 // "hidden-lg hidden-md hidden-sm hidden-xs styles-2"
 // col-sm-3 col-sm-offset-0 col-xs-10 col-xs-offset-1
+
+// <div class="hidden-lg hidden-md hidden-sm hidden-xs styles-2">
+// 	<div class=styles-block>
+// 		<img src=assets/img/styles-6.jpg alt>
+// 		<h4>Поп-арт</h4>
+// 		<a href="#">Подробнее</a>
+// 	</div>
+// </div>
